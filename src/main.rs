09669eac03196a476d86da3813f2a86d147ecf46
Rohit kumar barada <rohitbarada@gmail.com>
2024-05-02 14:55:06 +0530

    enum with default value

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
-struct Plot {
-    width :u32,
-    height:u32
+
+
+enum Direction{
+  up,
+  down,
+  left,
+  right
 }
-impl Plot {
-    fn area(&self)->u32{
-       return self.width*self.height;
+impl Direction {
+   fn default(&self)->isize{
+    match self {
+        Direction::up => 1,
+        Direction::down => 2,
+        Direction::left => 3,
+        Direction::right => 4
     }
-    fn perimeter(&self)->u32{
-        2*(self.width+self.height)
-    }
+   }
 }
 fn main(){
-    let plot1 = Plot {
-        width:32,
-        height:44
-    };
-    println!("width :{} ,height :{}, area : {}",plot1.width,plot1.height,plot1.area());
-    println!("width :{} ,height :{}, perimeter: {}",plot1.width,plot1.height,plot1.perimeter());
+    let north =Direction::up;
+let default_value = north.default();
+println!("{}",default_value)
 }
+

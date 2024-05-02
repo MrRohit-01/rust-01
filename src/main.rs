1e38ad582959363168a8b3e2ce987244f087e5b7
Rohit kumar barada <rohitbarada@gmail.com>
2024-05-02 14:36:18 +0530

    struct fn

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,15 @@ impl Plot {
     fn area(&self)->u32{
        return self.width*self.height;
     }
+    fn perimeter(&self)->u32{
+        2*(self.width+self.height)
+    }
 }
 fn main(){
     let plot1 = Plot {
         width:32,
         height:44
     };
-    println!("width :{} ,height :{}, area : {}",plot1.width,plot1.height,plot1.area())
+    println!("width :{} ,height :{}, area : {}",plot1.width,plot1.height,plot1.area());
+    println!("width :{} ,height :{}, perimeter: {}",plot1.width,plot1.height,plot1.perimeter());
 }
